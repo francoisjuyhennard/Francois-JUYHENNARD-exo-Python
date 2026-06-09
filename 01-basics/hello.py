e2fbfbc4710316320e543dfce622d415ca2af698
Your NameFrançois (francoisjuyhennard) <francois.juy.hennard@gmail.com>
2026-06-09 15:30:48 +0200

    hello.py

---
+++ 01-basics/hello.py
@@ -0,0 +1,7 @@
+print("Bonjour le monde !")
+print("Python", "c'est parti")
+
+prenom = input("Quel est ton prénom ? ")
+age = int(input("Quel âge as-tu ? "))
+
+print(f"Bonjour {prenom}, dans 10 ans tu auras {age+10} ans. !")
